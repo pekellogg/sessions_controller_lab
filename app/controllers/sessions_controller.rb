b851b16affd690e7d92951c65ed6d6692ad08fc8
Peyton Kellogg <pekellogg@hotmail.com>
2022-06-03 23:25:08 -0700

    add'l refactoring for presentation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,21 @@
 class SessionsController < ApplicationController
   before_action :check_login, only: [:edit, :update]
+  before_action :redirect_if_logged_in, only: [:new, :create]
 
   def new
   end
 
   def create
-    if !logged_in?
-      if !session_params[:name].nil?
-        if session_params[:name].is_a?(String) && !session_params[:name].empty?
-          session[:name] = session_params[:name]
-          redirect_to("/")
-        else
-          redirect_to(login_path)
-        end
+    redirect_if_logged_in
+    if !session_params[:name].nil?
+      if session_params[:name].is_a?(String) && !session_params[:name].empty?
+        session[:name] = session_params[:name]
+        redirect_to(:root)
       else
         redirect_to(login_path)
       end
     else
-      # flash[:alert] = "Please log out before creating a new account!"
-      # redirect_to(login_path)
-      redirect_to(:root)
+      redirect_to(login_path)
     end
   end
 
@@ -39,6 +35,10 @@ class SessionsController < ApplicationController
       flash[:alert] = "Please log in!"
       redirect_to(login_path)
     end
+  end
+
+  def redirect_if_logged_in
+    return redirect_to(:root) if logged_in?
   end
 
 end
